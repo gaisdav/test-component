fe93f0308020d52d49ca144a3b99d5ec62caaa76
GDavletkildin <gais.davletkildin@gmail.com>
2021-09-06 13:54:27 +0300

    remove Popover

--- src/Popover/Helper/styled.ts
+++
@@ -1,6 +0,0 @@
-import styled from 'styled-components';
-import { Typography } from '../../Typography';
-
-export const Title = styled(Typography).attrs({ variant: 'caption' })`
-  color: ${(props) => `${props.theme.custom.lightTheme.colors.typographyAndIcons.inverse.active} !important`};
-`;
